3c82e2a32a8930e134c637a9aea650dd3f95115f
hauserm <mhauser@o-s-k.de>
2025-12-05 10:20:31 +0100

    KI repetitive Aufgaben machen lassen (Struktur zum Auslesen der Datenbank erstellen)

--- cmanager_inventurverwaltung.cpp
+++ cmanager_inventurverwaltung.cpp
@@ -1,25 +1,262 @@
 #include "cmanager_inventurverwaltung.h"
 #include <QtSql/QSqlDatabase>
-#include <QMessagebox>
+#include <QMessageBox>
 #include <QSqlError>
+#include <QSqlQuery>
 
-cMANAGER_INVENTURVERWALTUNG::cMANAGER_INVENTURVERWALTUNG()
+cMANAGER_InventurVERWALTUNG::cMANAGER_InventurVERWALTUNG()
 {
     m_db = QSqlDatabase::addDatabase("QODBC");
-    m_db.setDatabaseName("Driver={SQL Server};Server=hertelt;database=Inventurverwaltung;username=dba;password=sqlosk");
+    m_db.setDatabaseName("Driver={SQL Server};Server=herteltlaptop;database=kisdb_demo_20230808;username=dba;password=sqlosk");
     if(!m_db.open())
     {
         QMessageBox msg;
-        msg.setWindowTitle("Fehler in cMANAGER_INVENTURVERWALTUNG::cMANAGER_INVENTURVERWALTUNG");
+        msg.setWindowTitle("Fehler in cMANAGER_InventurVERWALTUNG::cMANAGER_InventurVERWALTUNG");
         msg.setText(m_db.lastError().text());
         msg.exec();
     }
 }
 
-cMANAGER_INVENTURVERWALTUNG::~cMANAGER_INVENTURVERWALTUNG()
+cMANAGER_InventurVERWALTUNG::~cMANAGER_InventurVERWALTUNG()
 {
     if(m_db.isOpen())
     {
         m_db.close();
+    }
+}
+
+// --- Gegenstaende ---
+
+void cMANAGER_InventurVERWALTUNG::FillVecGegenstaende()
+{
+    QString strsql = ""; // SQL-Statement einfügen
+    QSqlQuery query(m_db);
+    query.prepare(strsql);
+    query.exec();
+    if(query.lastError().isValid())
+    {
+        QMessageBox msg;
+        msg.setWindowTitle("Fehler in cMANAGER_InventurVERWALTUNG::FillVecGegenstaende");
+        msg.setText(query.lastError().text());
+        msg.exec();
+        return;
+    }
+
+    m_vecGegenstaende.clear(); // Vektor vor dem Befüllen leeren
+
+    while(query.next())
+    {
+        // Auslesen der Daten und Hinzufügen zum Vektor
+    }
+}
+
+// --- Person (NEU) ---
+
+void cMANAGER_InventurVERWALTUNG::FillVecPerson()
+{
+    QString strsql = ""; // SQL-Statement einfügen
+    QSqlQuery query(m_db);
+    query.prepare(strsql);
+    query.exec();
+    if(query.lastError().isValid())
+    {
+        QMessageBox msg;
+        msg.setWindowTitle("Fehler in cMANAGER_InventurVERWALTUNG::FillVecPerson");
+        msg.setText(query.lastError().text());
+        msg.exec();
+        return;
+    }
+
+    m_vecPerson.clear();
+
+    while(query.next())
+    {
+        // Auslesen der Daten und Hinzufügen zum Vektor
+    }
+}
+
+// --- Rolle (NEU) ---
+
+void cMANAGER_InventurVERWALTUNG::FillVecRolle()
+{
+    QString strsql = ""; // SQL-Statement einfügen
+    QSqlQuery query(m_db);
+    query.prepare(strsql);
+    query.exec();
+    if(query.lastError().isValid())
+    {
+        QMessageBox msg;
+        msg.setWindowTitle("Fehler in cMANAGER_InventurVERWALTUNG::FillVecRolle");
+        msg.setText(query.lastError().text());
+        msg.exec();
+        return;
+    }
+
+    m_vecRolle.clear();
+
+    while(query.next())
+    {
+        // Auslesen der Daten und Hinzufügen zum Vektor
+    }
+}
+
+// --- Gruppe (NEU) ---
+
+void cMANAGER_InventurVERWALTUNG::FillVecGruppe()
+{
+    QString strsql = ""; // SQL-Statement einfügen
+    QSqlQuery query(m_db);
+    query.prepare(strsql);
+    query.exec();
+    if(query.lastError().isValid())
+    {
+        QMessageBox msg;
+        msg.setWindowTitle("Fehler in cMANAGER_InventurVERWALTUNG::FillVecGruppe");
+        msg.setText(query.lastError().text());
+        msg.exec();
+        return;
+    }
+
+    m_vecGruppe.clear();
+
+    while(query.next())
+    {
+        // Auslesen der Daten und Hinzufügen zum Vektor
+    }
+}
+
+// --- Status (NEU) ---
+
+void cMANAGER_InventurVERWALTUNG::FillVecStatus()
+{
+    QString strsql = ""; // SQL-Statement einfügen
+    QSqlQuery query(m_db);
+    query.prepare(strsql);
+    query.exec();
+    if(query.lastError().isValid())
+    {
+        QMessageBox msg;
+        msg.setWindowTitle("Fehler in cMANAGER_InventurVERWALTUNG::FillVecStatus");
+        msg.setText(query.lastError().text());
+        msg.exec();
+        return;
+    }
+
+    m_vecStatus.clear();
+
+    while(query.next())
+    {
+        // Auslesen der Daten und Hinzufügen zum Vektor
+    }
+}
+
+// --- Fach (NEU) ---
+
+void cMANAGER_InventurVERWALTUNG::FillVecFach()
+{
+    QString strsql = ""; // SQL-Statement einfügen
+    QSqlQuery query(m_db);
+    query.prepare(strsql);
+    query.exec();
+    if(query.lastError().isValid())
+    {
+        QMessageBox msg;
+        msg.setWindowTitle("Fehler in cMANAGER_InventurVERWALTUNG::FillVecFach");
+        msg.setText(query.lastError().text());
+        msg.exec();
+        return;
+    }
+
+    m_vecFach.clear();
+
+    while(query.next())
+    {
+        // Auslesen der Daten und Hinzufügen zum Vektor
+    }
+}
+
+// --- Eigenschaft (NEU) ---
+
+void cMANAGER_InventurVERWALTUNG::FillVecEigenschaft()
+{
+    QString strsql = ""; // SQL-Statement einfügen
+    QSqlQuery query(m_db);
+    query.prepare(strsql);
+    query.exec();
+    if(query.lastError().isValid())
+    {
+        QMessageBox msg;
+        msg.setWindowTitle("Fehler in cMANAGER_InventurVERWALTUNG::FillVecEigenschaft");
+        msg.setText(query.lastError().text());
+        msg.exec();
+        return;
+    }
+
+    m_vecEigenschaft.clear();
+
+    while(query.next())
+    {
+        // Auslesen der Daten und Hinzufügen zum Vektor
+    }
+}
+
+// --- Abteilung (NEU) ---
+
+void cMANAGER_InventurVERWALTUNG::FillVecAbteilung()
+{
+    QString strsql = ""; // SQL-Statement einfügen
+    QSqlQuery query(m_db);
+    query.prepare(strsql);
+    query.exec();
+    if(query.lastError().isValid())
+    {
+        QMessageBox msg;
+        msg.setWindowTitle("Fehler in cMANAGER_InventurVERWALTUNG::FillVecAbteilung");
+        msg.setText(query.lastError().text());
+        msg.exec();
+        return;
+    }
+
+    m_vecAbteilung.clear();
+
+    while(query.next())
+    {
+        // Auslesen der Daten und Hinzufügen zum Vektor
+    }
+}
+
+// --- Standort (NEU) ---
+
+void cMANAGER_InventurVERWALTUNG::FillVecStandort()
+{
+    QString strsql = "select * from STANDORT";
+    QSqlQuery query(m_db);
+    query.prepare(strsql);
+    query.exec();
+    if(query.lastError().isValid())
+    {
+        QMessageBox msg;
+        msg.setWindowTitle("Fehler in cMANAGER_InventurVERWALTUNG::FillVecStandort");
+        msg.setText(query.lastError().text());
+        msg.exec();
+        return;
+    }
+
+    m_vecStandort.clear();
+
+    structStandort standort;
+
+    while(query.next())
+    {
+        standort.iSTANDORT_ID = query.value("STANDORT_ID").toInt();
+        standort.strBESCHREIBUNG = query.value("BESCHREIBUNG").toString();
+        standort.strBUNDESLAND = query.value("BUNDESLAND").toString();
+        standort.strPLZ = query.value("PLZ").toString();
+        standort.strSTRASSE = query.value("STRASSE").toString();
+        standort.iHAUS_NR = query.value("HAUS_NR").toInt();
+        standort.strORT = query.value("ORT").toString();
+        standort.strHAUSZUSATZ = query.value("HAUSZUSATZ").toString();
+
+        m_vecStandort.push_back(standort);
     }
 }
--- cmanager_inventurverwaltung.h
+++ cmanager_inventurverwaltung.h
@@ -1,16 +1,147 @@
-#ifndef CMANAGER_INVENTURVERWALTUNG_H
-#define CMANAGER_INVENTURVERWALTUNG_H
+#ifndef CMANAGER_GegenstaendeVERWALTUNG_H
+#define CMANAGER_GegenstaendeVERWALTUNG_H
 
 #include <QSqlDatabase>
+#include <vector>
+#include <QString>
 
+// --- Strukturen ---
 
-class cMANAGER_INVENTURVERWALTUNG
+struct structGegenstand
 {
-public:
-    cMANAGER_INVENTURVERWALTUNG();
-    ~cMANAGER_INVENTURVERWALTUNG();
-protected:
-    QSqlDatabase m_db;
+    int iID = 0;
+    int iGruppeID = 0;
+    int iFachID = 0;
+    int iStatusID = 0;
+    int iEigenschaft = 0;
+    int iAbteilung = 0;
 };
 
-#endif // CMANAGER_INVENTURVERWALTUNG_H
+struct structRolle
+{
+    int iID = 0;
+    QString strBez = "";
+};
+
+struct structPerson
+{
+    int iID = 0;
+    QString strName = "";
+    QString strVorname = "";
+    int iRolle = 0;
+};
+
+struct structGruppe
+{
+    int iID = 0;
+    QString strBez = "";
+};
+
+struct structStatus
+{
+    int iID = 0;
+    QString strBez = "";
+    int iVerantwortlicherID = 0;
+};
+
+struct structFach
+{
+    int iID = 0;
+    QString strBez = "";
+};
+
+struct structEigenschaft
+{
+    int iID = 0;
+    QString strBez = "";
+};
+
+struct structAbteilung
+{
+    int iID = 0;
+    QString strBez = "";
+};
+
+struct structStandort
+{
+    int iSTANDORT_ID = 0;
+    QString strBESCHREIBUNG = "";
+    QString strBUNDESLAND = "";
+    QString strPLZ = "";
+    QString strSTRASSE = "";
+    int iHAUS_NR = 0;
+    QString strORT = "";
+    QString strHAUSZUSATZ = "";
+};
+
+// --- Typedefs für Vektoren ---
+
+typedef std::vector<structGegenstand> vecGegenstaende;
+typedef std::vector<structPerson> vecPerson;
+typedef std::vector<structRolle> vecRolle; // NEU
+typedef std::vector<structGruppe> vecGruppe;
+typedef std::vector<structStatus> vecStatus; // NEU
+typedef std::vector<structFach> vecFach; // NEU
+typedef std::vector<structEigenschaft> vecEigenschaft; // NEU
+typedef std::vector<structAbteilung> vecAbteilung; // NEU
+typedef std::vector<structStandort> vecStandort; // NEU
+
+
+class cMANAGER_InventurVERWALTUNG
+{
+public:
+    cMANAGER_InventurVERWALTUNG();
+    ~cMANAGER_InventurVERWALTUNG();
+    QSqlDatabase* Getdb() const { return const_cast<QSqlDatabase*>(&m_db);}
+
+    // Gegenstaende
+    vecGegenstaende* getvecGegenstaende() const { return const_cast<vecGegenstaende*>(&m_vecGegenstaende); }
+    void FillVecGegenstaende();
+
+    // Person
+    vecPerson* GetvecPerson() const { return const_cast<vecPerson*>(&m_vecPerson); }
+    void FillVecPerson(); // NEU
+
+    // Rolle
+    vecRolle* GetvecRolle() const { return const_cast<vecRolle*>(&m_vecRolle); } // NEU
+    void FillVecRolle(); // NEU
+
+    // Gruppe
+    vecGruppe* GetvecGruppe() const { return const_cast<vecGruppe*>(&m_vecGruppe); }
+    void FillVecGruppe(); // NEU
+
+    // Status
+    vecStatus* GetvecStatus() const { return const_cast<vecStatus*>(&m_vecStatus); } // NEU
+    void FillVecStatus(); // NEU
+
+    // Fach
+    vecFach* GetvecFach() const { return const_cast<vecFach*>(&m_vecFach); } // NEU
+    void FillVecFach(); // NEU
+
+    // Eigenschaft
+    vecEigenschaft* GetvecEigenschaft() const { return const_cast<vecEigenschaft*>(&m_vecEigenschaft); } // NEU
+    void FillVecEigenschaft(); // NEU
+
+    // Abteilung
+    vecAbteilung* GetvecAbteilung() const { return const_cast<vecAbteilung*>(&m_vecAbteilung); } // NEU
+    void FillVecAbteilung(); // NEU
+
+    // Standort
+    vecStandort* GetvecStandort() const { return const_cast<vecStandort*>(&m_vecStandort); } // NEU
+    void FillVecStandort(); // NEU
+
+protected:
+    QSqlDatabase m_db;
+
+    vecGegenstaende m_vecGegenstaende;
+    vecPerson m_vecPerson;
+    vecRolle m_vecRolle; // NEU
+    vecGruppe m_vecGruppe;
+    vecStatus m_vecStatus; // NEU
+    vecFach m_vecFach; // NEU
+    vecEigenschaft m_vecEigenschaft; // NEU
+    vecAbteilung m_vecAbteilung; // NEU
+    vecStandort m_vecStandort; // NEU
+};
+
+#endif // CMANAGER_GegenstaendeVERWALTUNG_H
